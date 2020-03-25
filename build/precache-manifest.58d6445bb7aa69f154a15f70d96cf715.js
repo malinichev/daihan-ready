d9cdb98eaeccf88149043a8e08f887c650e4102c
malinichev_s <malinichev_s@mail.ru>
2020-03-25 20:48:41 +0800

    «ver_1»

---
+++ build/precache-manifest.58d6445bb7aa69f154a15f70d96cf715.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d65b0960a5ae30a1b12553485014304d",
+    "url": "/react-redux/index.html"
+  },
+  {
+    "revision": "add02e55bbcb2ffb301f",
+    "url": "/react-redux/static/css/2.288f30ff.chunk.css"
+  },
+  {
+    "revision": "d8b2739d023793d7e8ab",
+    "url": "/react-redux/static/css/main.cf8f10e8.chunk.css"
+  },
+  {
+    "revision": "add02e55bbcb2ffb301f",
+    "url": "/react-redux/static/js/2.f38570ec.chunk.js"
+  },
+  {
+    "revision": "36ef42950c0061214f32dd51e2eadf78",
+    "url": "/react-redux/static/js/2.f38570ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8b2739d023793d7e8ab",
+    "url": "/react-redux/static/js/main.b3f73218.chunk.js"
+  },
+  {
+    "revision": "d520682ba562d4544503",
+    "url": "/react-redux/static/js/runtime-main.922c54c2.js"
+  },
+  {
+    "revision": "5cc57b04b43f7abb65c3c32113731d47",
+    "url": "/react-redux/static/media/Arrow-2.5cc57b04.svg"
+  },
+  {
+    "revision": "a09a6b21d06cbaa954be5aa08ac11d08",
+    "url": "/react-redux/static/media/Arrow.a09a6b21.svg"
+  },
+  {
+    "revision": "a8206dc953103c597690b94654b6813e",
+    "url": "/react-redux/static/media/about-page-bgTop.a8206dc9.jpg"
+  },
+  {
+    "revision": "6302c1bbf56c5ef7bbb4a0ed5438e6fd",
+    "url": "/react-redux/static/media/ant-rozetsky1.6302c1bb.jpg"
+  },
+  {
+    "revision": "e2d2acede05aef4cee39719e61ad23b1",
+    "url": "/react-redux/static/media/ant-rozetsky2.e2d2aced.jpg"
+  },
+  {
+    "revision": "0abfe3e1ee70f2e0605acdb1d7b60b14",
+    "url": "/react-redux/static/media/contact-page-imMap.0abfe3e1.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/react-redux/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/react-redux/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/react-redux/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/react-redux/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "5bc83e29e1c05176a55f05626dfd1c53",
+    "url": "/react-redux/static/media/slidAb.5bc83e29.jpg"
+  }
+]);
